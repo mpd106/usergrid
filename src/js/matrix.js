fcd52d3aecf23ff0ecd8f076bd314f924245ae71
Matt Dickens <mpd106@gmail.com>
2014-03-29 18:38:02 +0000

    Expose matrix by rows

--- src/js/matrix.js
+++ src/js/matrix.js
@@ -1,7 +1,36 @@
 /* global exports */
 
-exports.Matrix = function(data) {
+var my = {};
+
+exports.Matrix = function(data, numCols, numRows) {
     if (Object.getPrototypeOf(this) !== exports.Matrix.prototype) {
         throw new Error("Constructors must be called with new");
     }
+    
+    if (data.length !== numCols * numRows) {
+        throw new Error("Data length and numCols * numRows must match");
+    }
+    
+    my.data = data;
+    my.numCols = numCols;
+    my.numRows = numRows;
+};
+
+exports.Matrix.prototype.getRows = function() {
+    var rows = [],
+        rowIndex,
+        cols,
+        colIndex,
+        col,
+        index = 0;
+    
+    for (rowIndex = 0; rowIndex < my.numRows; rowIndex++) {
+        col = rows[rowIndex] = [];
+        for (colIndex = 0; colIndex < my.numCols; colIndex++) {
+            col[colIndex] = my.data[index];
+            index++;
+        }
+    }
+    
+    return rows;
 };
--- test/matrix-test.js
+++ test/matrix-test.js
@@ -8,16 +8,66 @@ describe("matrix", function() {
     
     describe('constructor', function() {
         it('should exist', function() {
-            var mat = new matrix.Matrix(data);
+            var mat = new matrix.Matrix(data, 3, 3);
         });
         
         it('should throw if new is omitted', function() {
-            should(function() { var mat = matrix.Matrix(data); }).throw(/new/);
+            should(function() { var mat = matrix.Matrix(data, 3, 3); }).throw(/new/);
         });
         
         it('should return a valid matrix', function() {
-            var mat = new matrix.Matrix(data);
+            var mat = new matrix.Matrix(data, 3, 3);
             should.exist(mat);
+        });
+        
+        it('should expect the correct number of elements', function() {
+            data.push(10);
+            should(function() {
+                var mat = new matrix.Matrix(data, 3, 3);
+            }).throw(/numCols \* numRows/);
+        });
+    });
+    
+    describe('getRows', function() {
+        var mat = new matrix.Matrix(data, 3, 3);
+        
+        it('should return an array', function() {
+            var rows = mat.getRows();
+            rows.should.be.an.instanceOf(Array);
+        });
+        
+        it('should have n elements', function() {
+            var rows = mat.getRows();
+            rows.length.should.equal(3);
+        });
+        
+        it('should have rows of the correct length', function() {
+            var rows = mat.getRows(),
+                numRows = rows.length,
+                expectedLength = 3,
+                rowIndex;
+            
+            for (rowIndex = 0; rowIndex < numRows; rowIndex++) {
+                rows[rowIndex].length.should.equal(expectedLength);
+            }
+        });
+        
+        it('should have elements in the right oder', function() {
+            var rows = mat.getRows(),
+                numRows = rows.length,
+                rowIndex,
+                numCols,
+                colIndex,
+                col,
+                index = 0;
+            
+            for (rowIndex = 0; rowIndex < numRows; rowIndex++) {
+                col = rows[rowIndex];
+                for (colIndex = 0; colIndex < col.length; colIndex++) {
+                    col[colIndex].should.equal(data[index]);
+                    index++;
+                }
+            }
         });
     });
 });
